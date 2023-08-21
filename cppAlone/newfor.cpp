6de839f2c3e917051c2dd3342dcbd3a043786faa
garfield-gray <garfield.gray.999@gmail.com>
2023-08-21 14:18:00 +0330

    reordering

---
+++ cppAlone/newfor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::cout<<"ro"<<std::endl;
+	std::vector<int> numbers = {1, 2, 3, 4, 5};
+	for (auto i : numbers)
+		std::cout<<i<<std::endl;
+
+
+}
